642bde446843dccd746d8552f02112c194b9217a
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-11-12 15:48:58 +0300

    add task model

---
+++ tasks/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Task(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Название задачи',
+        help_text='Укажите название задачи',
+    )
+    description = models.TextField(
+        verbose_name='Текст задачи',
+        help_text='Добавьте текст задачи',
+        **NULLABLE
+    )
+    image = models.ImageField(
+        upload_to='blog/images',
+        verbose_name='Изображение',
+        help_text='Загрузите изображение для задачи, если это необходимо',
+        **NULLABLE
+    )
+    file = models.FileField(
+        upload_to='blog/files',
+        verbose_name='Файл',
+        help_text='Загрузите файл для задачи, если это необходимо',
+        **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
