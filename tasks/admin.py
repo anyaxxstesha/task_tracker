61816f2c73cf16e8190f8d5b678cb7a6b69cdd96
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-11-12 16:10:41 +0300

    register model

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'views_amount')
+    list_filter = ('title',)
+    search_fields = ('title', 'description')
